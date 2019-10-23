3ad4f9cf8d1d25a2f9759ee3a87ca56ace6f45ba
Adam Mansfield <adammansfield37@gmail.com>
2019-10-23 11:27:35 -0400

    asdf

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ use rust_htslib::bcf::Read;
 fn main() {
     let mut vcf = bcf::Reader::from_path("src/test_data/tmp.vcf")
         .expect(&format!("Failed to open file"));
-    let num_samples = vcf.header().sample_count() as usize;
 
     for (rec_index, rec) in vcf.records().enumerate() {
         let mut record = rec.unwrap();
-        let tmp = record.format(b"FT").string().unwrap();
+        let sample_filters = record.format(b"FT").string().unwrap();
         let genotypes = record.genotypes().unwrap();
     }
 }
